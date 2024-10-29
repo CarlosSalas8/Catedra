abcaddf353c751dd528537b5f9ce01c2480b7a9b
CarlosSalas8 <salascarlosm18@gmail.com>
2024-10-29 17:11:32 -0500

    Summary
    Primer Commit

---
+++ src/app/components/director/home-director/home-director.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PeriodoService } from 'src/app/services/periodo.service';
+
+@Component({
+  selector: 'app-home-director',
+  templateUrl: './home-director.component.html',
+  styleUrls: ['./home-director.component.css']
+})
+export class HomeDirectorComponent implements OnInit {
+  activePeriod: any | null = null;
+
+  periodoActivo: any;
+
+  periodos: Observable<any[]> | undefined;
+
+  constructor(public periodoService: PeriodoService) {}
+
+  ngOnInit(): void {
+    this.periodoService.activePeriod$.subscribe(periodo => {
+      this.activePeriod = periodo;
+    });
+    this.setupMobileMenuToggle();
+  }
+
+  setupMobileMenuToggle(): void {
+    const menuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const menuIconClosed = menuButton?.children[2] as HTMLElement;
+    const menuIconOpened = menuButton?.children[3] as HTMLElement;
+
+    if (menuButton) {
+      menuButton.addEventListener('click', () => {
+        if (mobileMenu) {
+          mobileMenu.classList.toggle('hidden');
+        }
+        if (menuIconClosed && menuIconOpened) {
+          menuIconClosed.classList.toggle('hidden');
+          menuIconOpened.classList.toggle('hidden');
+        }
+      });
+    }
+  }
+
+}
